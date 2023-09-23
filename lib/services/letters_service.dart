4a3ae5dd8abc02dd026f19f9960baddce4da9f17
bryanbill <emobrya@gmail.com>
2023-09-23 12:10:55 +0300

    ft: integrates the backend letter service

--- lib/services/letters_service.dart
+++ lib/services/letters_service.dart
@@ -1,16 +1,17 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:chalk/chalk.dart';
 import 'package:flutter/material.dart';
 import 'package:letters/models/letter.dart';
 import 'package:http/http.dart' as http;
+import 'package:letters/utils/mixins.dart';
 
 // LetterService is a class that contains static methods to interact with the
 // letters API.
 //
 // Members are static coz we only need one instance of this class globally.
-class LetterService {
-  
-  // _baseUrl is a private static member that holds the base url for the API.
-  static const String _baseUrl = 'https://letters-2c7e5.firebaseio.com';
-
+class LetterService with Constants {
   // getLetters is a static method that returns a Future of List<Letter>.
   // It makes a GET request to the letters endpoint and returns a list of
   // letters.
@@ -19,20 +20,24 @@ class LetterService {
   // request was successful.
   static Future<(List<Letter>, bool)> getLetters() async {
     try {
-      final response = await http.get(Uri.parse('$_baseUrl/letters.json'));
-      final letters = <Letter>[];
-
+      final response =
+          await http.get(Uri.parse('${Constants.baseUrl}/letters'));
       if (response.statusCode != 200) throw Exception("Error Getting Letters");
 
-      final Map<String, dynamic> data = response.body as Map<String, dynamic>;
-      data.forEach((key, value) {
-        letters.add(Letter.fromJson(value as Map<String, dynamic>));
-      });
+      var data = List<Map<String, dynamic>>.from(jsonDecode(response.body));
+      final letters = <Letter>[];
+      for (var value in data) {
+        letters.add(Letter.fromJson(value));
+      }
 
       return (letters, true);
     } catch (e) {
-      debugPrint("Error Getting Letters: $e");
+      debugPrint("Error Getting Letters: $e".red());
       debugPrintStack();
+
+      // We can attempt to get letters from the local database if the request
+      // fails.
+
       return ([] as List<Letter>, false);
     }
   }
@@ -44,16 +49,45 @@ class LetterService {
   // request was successful.
   static Future<(Letter?, bool)> getLetter(String id) async {
     try {
-      final response = await http.get(Uri.parse('$_baseUrl/letters/$id.json'));
+      final response =
+          await http.get(Uri.parse('${Constants.baseUrl}/letters/$id'));
 
       if (response.statusCode != 200) throw Exception("Letter not found");
 
-      final Map<String, dynamic> data = response.body as Map<String, dynamic>;
-      final letter = Letter.fromJson(data);
+      final letter = Letter.fromJson(jsonDecode(response.body));
       return (letter, true);
     } catch (e) {
       debugPrint("Error Getting Letter: $e");
       debugPrintStack();
+
+      // We can attempt to get letter from the local database if the request
+      // fails.
+
+      return (null, false);
+    }
+  }
+
+  // createLetter is a static method that returns a Future of Letter and a
+  // boolean to indicate if the request was successful.
+  // It makes a POST request to the letters endpoint and returns a letter.
+
+  Future<(Letter?, bool)> postLetter(Letter letter) async {
+    try {
+      final response = await http.post(
+        Uri.parse('${Constants.baseUrl}/letters'),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: letter.toJson(),
+      );
+
+      if (response.statusCode != 201) throw Exception("Error Creating Letter");
+      var createdLetter = Letter.fromJson(jsonDecode(response.body));
+      return (createdLetter, true);
+    } catch (e) {
+      debugPrint("Error Creating Letter: $e");
+      debugPrintStack();
+
       return (null, false);
     }
   }
