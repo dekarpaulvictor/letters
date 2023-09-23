1c6cea32454016a66b3fbb415fd5b6887d1088b1
bryanbill <emobrya@gmail.com>
2023-09-23 14:15:23 +0300

    chore:exports create and view all letters screens

--- lib/presentation/presentation.dart
+++ lib/presentation/presentation.dart
@@ -1,6 +1,8 @@
 export 'screens/splash_screen.dart';
-export 'screens/letters_screen.dart';
+export 'screens/home_screen.dart';
 export 'screens/view_letter_screen.dart';
+export 'screens/create_letter_screen.dart';
+export 'screens/letters_screen.dart';
 
 export 'widgets/letter_card.dart';
 export 'widgets/sliverbar.dart';
