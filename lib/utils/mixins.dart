86c2ec93951919c6eb632d43400e7ece8b00851b
bryanbill <emobrya@gmail.com>
2023-09-23 15:52:18 +0300

    adds render base url

--- lib/utils/mixins.dart
+++ lib/utils/mixins.dart
@@ -1,5 +1,5 @@
 mixin Constants {
   // Your API base URL goes here
-  // Note: For local development use your host machine IP address
-  static const String baseUrl = "http://192.168.2.114:3000/api/v1";
+  // Note: For local development use your host machine IP address e.g http://192/168.1.1:3000/api/v1
+  static const String baseUrl = "https://letters-csow.onrender.com/api/v1";
 }
